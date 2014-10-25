c3717f0ed85caa2763180f140ed5dd39fbaa7710
Stefan K <uplink.coder@gmail.com>
2014-10-25 10:52:41 +0000

    makeing test0168 testing more things

--- tests/test0168.d
+++ tests/test0168.d
@@ -1,26 +1,25 @@
 //T compiles:yes
 //T has-passed:yes
 //T retval:42
-//? Tests TernaryOperator
+//? Tests TernaryOperator and foreach and ArrayLength and vrp
 
 import std.stdio;
 
 int main() {
-	auto s = 0;
-	ubyte right = cast(ubyte) 42;
-	bool b_wrong = cast(bool) 0;
+	ubyte right = 42;
+	bool b_wrong = 0;
 	int i_wrong;
 	uint ui_wrong = 0;
 
 	bool[12] bs;
 	for(int i=0;i<bs.length;i++) {
-		*(&bs[0]+i) = cast(bool) (i%2);
+		(bs[0]) = i%2;
 	}
 
 	int ret=18;
 
 	foreach(b;bs) {
-		(b||(true&&false))?(ret+=7):(ret-=3);
+		(b || true && false) ? (ret+=7) : (ret-=3);
 	}
 	
 	if ((i_wrong?right:b_wrong)?5:ret != 42) {
