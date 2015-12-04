dff4aa7a1136905de7ce55cf6aba6314336755ea
Stefan Koch <uplink.coder@googlemail.com>
2015-12-04 18:46:36 +0000

    Add easy overload to compile sourceBuffers

--- sdc/src/sdc/sdc.d
+++ sdc/src/sdc/sdc.d
@@ -76,6 +76,7 @@ final class SDC {
 	void compile(string filename) {
 		modules ~= semantic.add(filename);
 	}
+
 	/// The string needs to be 0-terminated!
 	/// The terminator needs to be included in bufferLength!
 	bool compileBuffer(const char* bufferPtr, const size_t bufferLength, const char* namePtr, const size_t nameLength) {
