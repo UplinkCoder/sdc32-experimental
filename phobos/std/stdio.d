caaee139678d7a7bb0e1c8dc4652bfb2df899d52
Stefan Koch <uplink.coder@googlemail.com>
2015-12-10 15:07:38 +0000

    make stdio into template to workaround linking bugs

--- phobos/std/stdio.d
+++ phobos/std/stdio.d
@@ -1,9 +1,27 @@
 /// Part of a 'mock' phobos used for testing. Not intended for real use.
 module std.stdio;
-void writeln(string s) {
+
+void writeln(T)(T s) {
+	static if (is(T:string)) {
+		printf("%.*s\n".ptr, s.length, s.ptr);
+	} else static if (is(T:bool)) {
+		string str = (s ? "true" : "false");
+		printf("%.*s\n".ptr, str.length, str.ptr);
+		//TODO make this work! it has to do with instanciating
+		// The same template inside a template (maybe just ITFI)
+	//	writeln(str);
+	} else static if (is(T:int)) {
+		printf("%d", s);
+	}
+
+	return ;
+}
+/+
+void writeln(T:string)(T s) {
 	printf("%.*s\n".ptr, s.length, s.ptr);
 }
 
-void writeln(int i) {
+void writeln(T:int)(T i) {
 	printf("%d\n".ptr, i);
 }
++/
