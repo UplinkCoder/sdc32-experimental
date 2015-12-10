ee31d48587f47a6fd78aeb56396fb3cabbd62927
Stefan Koch <uplink.coder@googlemail.com>
2015-12-10 15:40:53 +0000

    make ArrayLiterals r-values

--- libd/src/d/semantic/expression.d
+++ libd/src/d/semantic/expression.d
@@ -1299,7 +1299,7 @@ public:
 			.reduce!((t1, t2) => getCommonType(pass, e.location, t1, t2))
 			.getArray(cast(uint)values.length);
 
-		return build!ArrayLiteral(e.location, type, values);
+		return getTemporary(build!ArrayLiteral(e.location, type, values));
 	}
 	
 	Expression visit(Lambda e) {
